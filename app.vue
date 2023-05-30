0077856f05770870bc0c5857a035fb425ea48c47
derushio <derushio@aiqlab.com>
2023-05-31 01:57:50 +0900

    serverFunctionの型保管が効かないのを修正

--- app.vue
+++ app.vue
@@ -1,9 +1,19 @@
 <template>
   <MainLayout>
     <q-btn @click="$q.notify('test')">test</q-btn>
+    {{ serverFunctionTestResult }}
   </MainLayout>
 </template>
 
 <script setup lang="ts">
 import MainLayout from '@/layouts/MainLayout.vue';
+
+import type * as functions from '#build/server-fn';
+
+const serverFns: typeof functions = useServerFunctions();
+const serverFunctionTestResult = ref<string[]>();
+
+onMounted(async () => {
+  serverFunctionTestResult.value = await serverFns.serverFunctionTest();
+});
 </script>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,6 +14,7 @@ export default defineNuxtConfig({
     '@/': './',
     '$/': './common/',
     '%/': './prisma/',
+    '#build/': './.nuxt/',
   },
   ...{
     quasar: {
