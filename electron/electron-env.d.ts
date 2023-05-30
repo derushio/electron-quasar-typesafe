ce648455febd4254c02737a95eed1f6f03de1948
derushio <derushio@aiqlab.com>
2023-05-30 21:48:26 +0900

    setup: electron

---
+++ electron/electron-env.d.ts
@@ -0,0 +1,8 @@
+/* eslint-disable */
+
+declare namespace NodeJS {
+  interface ProcessEnv {
+    APP_PORT: string;
+    APP_URL: string;
+  }
+}
---
+++ electron/electron-main.ts
@@ -0,0 +1,66 @@
+import dotenv from 'dotenv';
+import { BrowserWindow, app, nativeTheme } from 'electron';
+import os from 'os';
+import path from 'path';
+
+dotenv.config();
+
+// needed in case process is undefined under Linux
+const platform = process.platform || os.platform();
+
+try {
+  if (platform === 'win32' && nativeTheme.shouldUseDarkColors === true) {
+    require('fs').unlinkSync(
+      path.join(app.getPath('userData'), 'DevTools Extensions'),
+    );
+  }
+} catch (_) {}
+
+let mainWindow: BrowserWindow | undefined;
+
+function createWindow() {
+  /**
+   * Initial window options
+   */
+  mainWindow = new BrowserWindow({
+    icon: path.resolve(__dirname, 'icons/icon.png'), // tray icon
+    width: 1000,
+    height: 600,
+    useContentSize: true,
+    webPreferences: {
+      contextIsolation: true,
+      // More info: https://v2.quasar.dev/quasar-cli-webpack/developing-electron-apps/electron-preload-script
+      preload: path.resolve(__dirname, 'electron-preload.ts'),
+    },
+  });
+
+  // void mainWindow.loadURL(process.env.APP_URL);
+
+  if (process.env.DEBUGGING) {
+    // if on DEV or Production with debug enabled
+    mainWindow.webContents.openDevTools();
+  } else {
+    // we're on production; no access to devtools pls
+    mainWindow.webContents.on('devtools-opened', () => {
+      mainWindow?.webContents.closeDevTools();
+    });
+  }
+
+  mainWindow.on('closed', () => {
+    mainWindow = undefined;
+  });
+}
+
+void app.whenReady().then(createWindow);
+
+app.on('window-all-closed', () => {
+  if (platform !== 'darwin') {
+    app.quit();
+  }
+});
+
+app.on('activate', () => {
+  if (mainWindow === undefined) {
+    createWindow();
+  }
+});
