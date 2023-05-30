7893a06b8035a9c5c06abee97bebc1b1ea6100cb
derushio <derushio@aiqlab.com>
2023-05-30 21:30:09 +0900

    fix build errors

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   modules: ['nuxt-server-fn'],
+  build: {
+    transpile: ['nuxt-server-fn'],
+  },
 });
-
-declare function defineNuxtConfig(arg0: any): any;
