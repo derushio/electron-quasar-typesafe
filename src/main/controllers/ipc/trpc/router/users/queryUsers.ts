f78ff287236e0003f7db87e2f65c67965aeffde4
derushio <derushio@aiqlab.com>
2023-06-11 00:28:02 +0900

    tsconfigエラーの解決

--- src/main/controllers/ipc/trpc/router/users/queryUsers.ts
+++ src/main/controllers/ipc/trpc/router/users/queryUsers.ts
@@ -2,7 +2,6 @@ import { t } from '#/controllers/ipc/trpc';
 import { responseList } from '#/controllers/ipc/trpc/router/response';
 import { usersResource } from '#/controllers/ipc/trpc/router/users';
 import { pc } from '#/repositories/prisma';
-import { sleep } from '$/utils/sleep';
 import { z } from 'zod';
 
 export const QueryUsersRequestZod = z.object({
