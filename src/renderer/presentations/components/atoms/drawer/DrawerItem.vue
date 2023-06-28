f492141aaf0b0a1b7b6dd931c99b77205278673c
derushio <derushio@aiqlab.com>
2023-06-29 01:39:53 +0900

    便利コンポーネント内包

---
+++ src/renderer/presentations/components/atoms/drawer/DrawerItem.vue
@@ -0,0 +1,9 @@
+<template>
+  <q-item clickable v-ripple>
+    <q-item-section>
+      <q-item-label class="rcsknm-min-ana text-h5 text-red-7">
+        <slot />
+      </q-item-label>
+    </q-item-section>
+  </q-item>
+</template>
---
+++ src/renderer/presentations/components/atoms/util/BlurQImg.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <q-img
+      :src="src"
+      :fit="fit"
+      :style="{
+        width: '100%',
+        height: '100%',
+        position: 'relative',
+        zIndex: 1,
+      }"
+    />
+    <div
+      :class="`absolute-full ${effectClass}`"
+      :style="{
+        zIndex: 2,
+      }"
+    />
+  </div>
+</template>
+
+<script lang="ts" setup>
+const props = defineProps<{
+  src?: string;
+  fit?: 'fill' | 'cover' | 'contain' | 'none' | 'scale-down' | undefined;
+  effectClass?: string;
+}>();
+props;
+
+// const emit = defineEmits<{}>();
+// emit;
+</script>
