b025f6ef106ffdbbbc103b37a892f616b02ef9bf
derushio <derushio@aiqlab.com>
2023-11-08 22:55:26 +0900

    細かい調整

--- tests/components/specs/DrawerItem.spec.ts
+++ tests/components/specs/DrawerItem.spec.ts
@@ -1,11 +1,11 @@
 import DrawerItemVue from '@/presentations/components/atoms/drawer/DrawerItem.vue';
 
-import { shallowMount } from '@vue/test-utils';
+import { mount } from '@vue/test-utils';
 import { expect, test } from 'vitest';
 
 test('DrawerItemVue', async () => {
   const text = 'test';
-  const drawerItem = shallowMount(DrawerItemVue, {
+  const drawerItem = mount(DrawerItemVue, {
     slots: {
       default: text,
     },
--- tests/renderer.setup.ts
+++ tests/renderer.setup.ts
@@ -1,3 +1,4 @@
+import { installQuasarPlugin } from '@quasar/quasar-app-extension-testing-unit-vitest';
 import { vi } from 'vitest';
 
 vi.mock('@trpc/client', () => ({
@@ -9,3 +10,5 @@ vi.mock('vue-query', () => ({
   useQuery: vi.fn(),
   useMutation: vi.fn(),
 }));
+
+installQuasarPlugin();
