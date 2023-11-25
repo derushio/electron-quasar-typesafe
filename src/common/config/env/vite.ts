79cd3fcac216369377c9e0bd870de32f8ee8fc4e
derushio <derushio@aiqlab.com>
2023-11-26 00:31:05 +0900

    envファイルの読み方が良くないので修正

--- src/common/config/env/vite.ts
+++ src/common/config/env/vite.ts
@@ -1,16 +1,22 @@
-export const Env = {
-  VITE_BUILDED: (import.meta.env.VITE_BUILDED ?? 'false') === 'true',
+let Env: Record<string, unknown> = {};
 
-  VITE_RENDERER_ENV_EXAMPLE: import.meta.env.VITE_RENDERER_ENV_EXAMPLE ?? '',
-  VITE_ENV_EXAMPLE: import.meta.env.VITE_ENV_EXAMPLE ?? '',
-  VITE_RENDERER_BACKEND_PORT: parseInt(
-    import.meta.env.VITE_RENDERER_BACKEND_PORT ?? '8020',
-  ),
+try {
+  Env = {
+    VITE_BUILDED: (import.meta.env.VITE_BUILDED ?? 'false') === 'true',
 
-  VITE_DATABASE_HOST: import.meta.env.VITE_DATABASE_HOST ?? '',
-  VITE_DATABASE_USER: import.meta.env.VITE_DATABASE_USER ?? '',
-  VITE_DATABASE_PASSWORD: import.meta.env.VITE_DATABASE_PASSWORD ?? '',
-  VITE_DATABASE_PORT: import.meta.env.VITE_DATABASE_PORT ?? '',
-  VITE_DATABASE_NAME: import.meta.env.VITE_DATABASE_NAME ?? '',
-  VITE_DATABASE_URL: import.meta.env.VITE_DATABASE_URL ?? '',
-};
+    VITE_RENDERER_ENV_EXAMPLE: import.meta.env.VITE_RENDERER_ENV_EXAMPLE ?? '',
+    VITE_ENV_EXAMPLE: import.meta.env.VITE_ENV_EXAMPLE ?? '',
+    VITE_RENDERER_BACKEND_PORT: parseInt(
+      import.meta.env.VITE_RENDERER_BACKEND_PORT ?? '8020',
+    ),
+
+    VITE_DATABASE_HOST: import.meta.env.VITE_DATABASE_HOST ?? '',
+    VITE_DATABASE_USER: import.meta.env.VITE_DATABASE_USER ?? '',
+    VITE_DATABASE_PASSWORD: import.meta.env.VITE_DATABASE_PASSWORD ?? '',
+    VITE_DATABASE_PORT: import.meta.env.VITE_DATABASE_PORT ?? '',
+    VITE_DATABASE_NAME: import.meta.env.VITE_DATABASE_NAME ?? '',
+    VITE_DATABASE_URL: import.meta.env.VITE_DATABASE_URL ?? '',
+  };
+} catch {}
+
+export { Env };
