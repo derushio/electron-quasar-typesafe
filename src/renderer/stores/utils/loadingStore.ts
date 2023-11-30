cf16392adad54ff384907e9c26e1e5fb3b1b68d0
derushio <derushio@aiqlab.com>
2023-12-01 02:47:40 +0900

    なぜかわからんがSSR時のローディングが改善した

--- src/renderer/stores/utils/loadingStore.ts
+++ src/renderer/stores/utils/loadingStore.ts
@@ -1,36 +1,17 @@
 import { defineStore } from 'pinia';
 import { useQuasar } from 'quasar';
-import { ref } from 'vue';
 
 export const useLoadingStore = defineStore('loading', () => {
   const q = useQuasar();
 
-  // loadingの呼び出し回数を管理
-  const nests = ref(0);
-
-  const keyMaps = ref<Map<string, number>>(new Map());
-
   const actions = {
     show: (key?: string) => {
-      q.loading.show();
-      nests.value++;
-      if (key) {
-        keyMaps.value.set(key, (keyMaps.value.get(key) ?? 0) + 1);
-      }
+      q.loading.show({
+        group: key,
+      });
     },
     hide: (key?: string) => {
-      if (key) {
-        const keyCount = keyMaps.value.get(key) ?? 0;
-        keyMaps.value.delete(key);
-        nests.value -= keyCount;
-      } else {
-        nests.value--;
-      }
-
-      if (nests.value <= 0) {
-        q.loading.hide();
-        nests.value = 0;
-      }
+      q.loading.hide(key);
     },
     async doLoadingAction<T>(fn: () => T) {
       try {
@@ -45,5 +26,5 @@ export const useLoadingStore = defineStore('loading', () => {
 
   const getters = {};
 
-  return { nests, actions, getters };
+  return { actions, getters };
 });
