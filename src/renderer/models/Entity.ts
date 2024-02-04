f252a6e31e41f363b061cc1cb654b17fb9b42e87
derushio <derushio@aiqlab.com>
2024-02-04 17:49:46 +0900

    Entityの整理

--- src/renderer/models/Entity.ts
+++ src/renderer/models/Entity.ts
@@ -2,27 +2,28 @@
  * ModelのEntityのベースクラス
  */
 export abstract class Entity {
-  public static async getById(): Promise<EntityNullableResult<Entity>> {
-    throw new Error('Method not implemented.');
-  }
-  public static async query<T>(args: T): Promise<EntityListResult<Entity>> {
-    args;
-    throw new Error('Method not implemented.');
-  }
+  /** public static系のメソッドはOverrideできないためコメントアウトして置いておく */
+  // public static async getById(id: string): Promise<EntityNullableResult<Entity>> {
+  //   throw new Error('Method not implemented.');
+  // }
+  // public static async query<T>(args: T): Promise<EntityListResult<Entity>> {
+  //   args;
+  //   throw new Error('Method not implemented.');
+  // }
 
   public abstract save(): Promise<EntityResult<this>>;
   public abstract delete(): Promise<void>;
 }
 
 export interface EntityResult<T> {
-  value: T;
+  item: T;
 }
 
 export interface EntityNullableResult<T> {
-  value: T | undefined;
+  item: T | undefined;
 }
 
 export interface EntityListResult<T> {
-  values: T[];
+  items: T[];
   allCount: number;
 }
