dc68d1a29ba45e140abe8ba3d1f3664e179e67dc
derushio <derushio@gmail.com>
2024-05-17 19:16:06 +0900

    japaneseDateLocaleFormats

--- app/src/common/utils/dfUtils.ts
+++ app/src/common/utils/dfUtils.ts
@@ -2,16 +2,28 @@ import { ja } from 'date-fns/locale';
 
 /**
  * date-fns format DBなどショートフォーマット
+ * yyyy-MM-dd
  */
 export const dateShortFormats = ['yyyy-MM-dd', { locale: ja }] as const;
 
 /**
  * date-fns format 日本向けショートフォーマット
+ * yyyy/MM/dd
  */
 export const japaneseDateShortFormats = ['yyyy/MM/dd', { locale: ja }] as const;
 
 /**
+ * date-fns format 日本向けショートフォーマット
+ * yyyy年M月d日
+ */
+export const japaneseDateLocaleFormats = [
+  'yyyy年M月d日',
+  { locale: ja },
+] as const;
+
+/**
  * date-fns format 日本向けDateTimeフォーマット
+ * yyyy/MM/dd (eee) HH:mm
  */
 export const japaneseDateTimeFormats = [
   'yyyy/MM/dd (eee) HH:mm',
@@ -20,6 +32,7 @@ export const japaneseDateTimeFormats = [
 
 /**
  * date-fns format 日本向けDate曜日付きフォーマット
+ * yyyy/MM/dd (eee)
  */
 export const japaneseDateFormats = [
   'yyyy/MM/dd (eee)',
@@ -28,6 +41,7 @@ export const japaneseDateFormats = [
 
 /**
  * date-fns format 日本向けDateTimeフォーマット
+ * HH:mm
  */
 export const japaneseTimeFormats = ['HH:mm', { locale: ja }] as const;
 
